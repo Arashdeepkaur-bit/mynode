b93f3e3663112001d7f402e62a0e926de1b643e9
Arash <sainiarash555@gmail.com>
2024-11-28 11:38:41 +0530

    SQL

---
+++ node2/fifth.js
@@ -0,0 +1,64 @@
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "june2024"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    var sql = "select * from customers";
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
+
+
+
+
+
+
+/*
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers ORDER BY name", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
+// delete
+con.connect(function(err) {
+  if (err) throw err;
+  var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Number of records deleted: " + result.affectedRows);
+  });
+});
+// updateCommands
+con.connect(function(err) {
+  if (err) throw err;
+  var sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+  });
+});
+
+// limit 5
+
+con.connect(function(err) {
+  if (err) throw err;
+  var sql = "SELECT * FROM customers LIMIT 5";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
+
+*/
